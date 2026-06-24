a350cfd302efafe88281cc863997762562470ca8
CuongManhLe230104 <lemanhcuong230104@gmail.com>
2026-06-24 16:04:43 +0700

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,6 +8,8 @@ const api = axios.create({
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
+    // Bypass ngrok interstitial warning page for API calls
+    'ngrok-skip-browser-warning': 'true',
   },
 })
 
